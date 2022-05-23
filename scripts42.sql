6fcefae890c8c1b5ad18e431caab0422b7227482
dispersion <anyta1901@gmail.com>
2022-05-23 20:50:33 +0300

    Решение - управление бд + поправки

--- scripts42.sql
+++ scripts42.sql
@@ -1,7 +1,7 @@
 alter table student
     add constraint age check ( age > 16 );
 alter table faculty
-    add constraint text unique (color, name);
+    add constraint color_name_unique unique (color, name);
 alter table student
     add constraint unique_name unique (name) ;
 alter table student
--- scripts422.sql
+++ scripts422.sql
@@ -1,9 +1,5 @@
-create table person (id numeric, name varchar, age numeric,  drive_licence BOOLEAN );
-create table vehicle (id numeric, brand varchar, moder varchar, price numeric);
-alter table person
-    add primary key (name);
-alter table vehicle
-    add primary key (brand, moder);
+create table person (id numeric, name varchar primary key, age numeric,  drive_licence BOOLEAN );
+create table vehicle (id numeric, brand varchar primary key, moder varchar primary key, price numeric);
 select person.id, person.name, person.drive_licence, vehicle.id, vehicle.brand, vehicle.moder
 from vehicle
          left join person on person.id = vehicle.id;
